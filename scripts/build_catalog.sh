5b2c912cc4f397efe48fd792209f06c00b25f5b2
R3hankhan <rehan.irfan2002@gmail.com>
2024-10-15 15:52:43 +0530

    Update build_catalog.sh

--- scripts/build_catalog.sh
+++ scripts/build_catalog.sh
@@ -1,37 +1,35 @@
 #!/usr/bin/env bash
 # Builds the OLM catalog index and pushes it to quay.io.
-#
-# To push to your own registry, override the IMG_REGISTRY_HOST, IMG_REGISTRY_ORG, OPERATOR_NAME, and TAG env vars,
-# i.e:
-#   IMG_REGISTRY_HOST=quay.io IMG_REGISTRY_ORG=yourusername OPERATOR_NAME=authorino-operator TAG="latest 0e972a42f51453a8cea5e6df7f8f6ce6eb1b4075" ./script/build_catalog.sh
-#
-# REQUIREMENTS:
-#  * A valid login session to a container registry.
-#  * Docker
-#  * opm
 
 # Iterate over tag list, i.e., latest 0e972a42f51453a8cea5e6df7f8f6ce6eb1b4075
 IFS=' ' read -r -a tags <<< "$TAG"
 
-# Build & push catalog images for each architecture and tag.
+# Use the first tag to create the manifest and push images.
+first_tag="${tags[0]}"
+
+# Build & push catalog images for each architecture.
 for arch in amd64 ppc64le arm64 s390x; do
-  for tag in "${tags[@]}"; do
-    opm index add --build-tool docker \
-      --tag "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${tag}-${arch}" \
-      --bundles "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-bundle:${tag}" \
-      --binary-image "quay.io/operator-framework/opm:v1.28.0-${arch}"
-    
-    docker push "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${tag}-${arch}"
-  done
+  opm index add --build-tool docker \
+    --tag "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${first_tag}-${arch}" \
+    --bundles "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-bundle:${first_tag}" \
+    --binary-image "quay.io/operator-framework/opm:v1.28.0-${arch}"
+  
+  docker push "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${first_tag}-${arch}"
 done
 
-# Create and push a multi-architecture manifest for each tag.
-for tag in "${tags[@]}"; do
+# Create and push a multi-architecture manifest for the first tag.
+docker manifest create --amend "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${first_tag}" \
+  "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${first_tag}-amd64" \
+  "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${first_tag}-arm64" \
+  "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${first_tag}-ppc64le" \
+  "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${first_tag}-s390x"
+
+docker manifest push "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${first_tag}"
+
+# Annotate and push the same manifest for other tags.
+for tag in "${tags[@]:1}"; do
   docker manifest create --amend "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${tag}" \
-    "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${tag}-amd64" \
-    "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${tag}-arm64" \
-    "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${tag}-ppc64le" \
-    "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${tag}-s390x"
-  
+    --amend "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${first_tag}"
+
   docker manifest push "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${tag}"
 done
