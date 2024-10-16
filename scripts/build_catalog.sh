60e9a23e8871b9c353ada528f40a88499f8b9588
R3hankhan <rehan.irfan2002@gmail.com>
2024-10-16 10:21:20 +0530

    Update build_catalog.sh

--- scripts/build_catalog.sh
+++ scripts/build_catalog.sh
@@ -3,17 +3,32 @@
 
 # Iterate over tag list, i.e., latest 0e972a42f51453a8cea5e6df7f8f6ce6eb1b4075
 IFS=' ' read -r -a tags <<< "$TAG"
-
+PROJECT_DIR=$(dirname "$(realpath "$0")")
+CATALOG_FILE="${PROJECT_DIR}/catalog/authorino-operator-catalog/operator.yaml"
+CATALOG_DOCKERFILE="${PROJECT_DIR}/catalog/authorino-operator-catalog.Dockerfile"
 # Use the first tag to create the manifest and push images.
 first_tag="${tags[0]}"
+BUNDLE_IMG="${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-bundle:${first_tag}"
 
 # Build & push catalog images for each architecture.
 for arch in amd64 ppc64le arm64 s390x; do
-  opm index add --build-tool docker \
-    --tag "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${first_tag}-${arch}" \
-    --bundles "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-bundle:${first_tag}" \
-    --binary-image "quay.io/operator-framework/opm:v1.28.0-${arch}"
+  rm -rf "${PROJECT_DIR}/catalog/authorino-operator-catalog"
+  rm -rf "${PROJECT_DIR}/catalog/authorino-operator-catalog.Dockerfile"
+  mkdir -p "${PROJECT_DIR}/catalog/authorino-operator-catalog"
+  cd "${PROJECT_DIR}/catalog" && opm generate dockerfile authorino-operator-catalog -i "quay.io/operator-framework/opm:v1.28.0-${arch}"
   
+  echo "************************************************************"
+  echo "Build authorino operator catalog"
+  echo "BUNDLE_IMG                  = ${BUNDLE_IMG}"
+  echo "CHANNELS                    = ${CHANNELS}"
+  echo "************************************************************"
+  echo
+  echo "Please check this matches your expectations and override variables if needed."
+  echo
+  
+  "${PROJECT_DIR}/utils/generate-catalog.sh" "${OPM}" "${YQ}" "${BUNDLE_IMG}" "$@" "${CHANNELS}"
+  
+  docker build "${PROJECT_DIR}/catalog" -f "${CATALOG_DOCKERFILE}" -t "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${first_tag}-${arch}"
   docker push "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${first_tag}-${arch}"
 done
 
@@ -30,6 +45,6 @@ docker manifest push "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-
 for tag in "${tags[@]:1}"; do
   docker manifest create --amend "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${tag}" \
     --amend "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${first_tag}"
-
+  
   docker manifest push "${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${tag}"
 done
