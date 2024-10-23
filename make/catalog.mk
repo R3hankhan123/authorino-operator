77b8969e7588cfed48ceb2d35b66aa546de746f3
R3hankhan <rehan.irfan2002@gmail.com>
2024-10-23 15:50:07 +0530

    Update catalog.mk

--- make/catalog.mk
+++ make/catalog.mk
@@ -58,11 +58,20 @@ catalog-multiarch: $(OPM) ## Generate catalog content and validate for multiple 
 		mkdir -p $(PROJECT_DIR)/catalog/authorino-operator-catalog; \
 		echo "Creating Dockerfile"; \
 		cd $(PROJECT_DIR)/catalog && $(OPM) generate dockerfile authorino-operator-catalog -i "quay.io/operator-framework/opm:v1.28.0-$$ARCH"; \
-		$(MAKE) $(CATALOG_FILE) BUNDLE_IMG=$(BUNDLE_IMG); \
+		@echo "************************************************************"
+		@echo "Build authorino operator catalog"
+		@echo
+		@echo "BUNDLE_IMG = $(BUNDLE_IMG)"
+		@echo "CHANNELS   = $(CHANNELS)"
+		@echo "************************************************************"
+		@echo
+		@echo "Please check this matches your expectations and override variables if needed."
+		@echo
+		$(PROJECT_DIR)/utils/generate-catalog.sh $(OPM) $(YQ) $(BUNDLE_IMG) $@ $(CHANNELS); \
 		cd $(PROJECT_DIR)/catalog && $(OPM) validate authorino-operator-catalog; \
 		CATALOG_IMG_MULTI=$$CATALOG_IMG_MULTI_BASE:$(first_tag)-$$ARCH; \
-		$(MAKE) catalog-build-multi IMG=$$CATALOG_IMG_MULTI; \
-		$(MAKE) catalog-push IMG=$$CATALOG_IMG_MULTI; \
+		docker build $(PROJECT_DIR)/catalog -f $(PROJECT_DIR)/catalog/authorino-operator-catalog.Dockerfile -t $$CATALOG_IMG_MULTI; \
+		docker push $$CATALOG_IMG_MULTI; \
 	done
 
 	@echo "Creating multi-arch manifest for tag: $(first_tag)"
