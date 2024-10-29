47c73df4b58abcbdbe25f82bd2f4baf6fa068881
R3hankhan <rehan.irfan2002@gmail.com>
2024-10-29 16:22:58 +0530

    Update build_catalog.sh

--- script/build_catalog.sh
+++ script/build_catalog.sh
@@ -13,7 +13,8 @@ for arch in amd64 ppc64le arm64 s390x; do
   make catalog-multiarch arch="${arch}"
   image_tag="${IMG_REGISTRY_HOST}/${IMG_REGISTRY_ORG}/${OPERATOR_NAME}-catalog:${first_tag}-${arch}"
   make catalog-build-multi IMG="${image_tag}"
-  #docker push "${image_tag}" &
+  docker push "${image_tag}" &
+  wait
 done
 
 # Create and push multi-architecture manifest
