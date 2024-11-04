7e6d85be659917ba2bf3d423da8abc0f603754b3
R3hankhan <rehan.irfan2002@gmail.com>
2024-11-04 19:34:41 +0530

    Update catalog.mk

--- make/catalog.mk
+++ make/catalog.mk
@@ -2,26 +2,26 @@
 
 # The image tag given to the resulting catalog image (e.g. make catalog-build CATALOG_IMG=example.com/operator-catalog:v0.2.0).
 CATALOG_IMG ?= $(IMAGE_TAG_BASE)-catalog:$(IMAGE_TAG)
+OPM_VERSION =? 1.48.0
+CATALOG_ARCH ?= amd64
 
 CATALOG_FILE = $(PROJECT_DIR)/catalog/authorino-operator-catalog/operator.yaml
 CATALOG_DOCKERFILE = $(PROJECT_DIR)/catalog/authorino-operator-catalog.Dockerfile
 
-# Generate catalog Dockerfile for single architecture.
 $(CATALOG_DOCKERFILE): $(OPM)
 	-mkdir -p $(PROJECT_DIR)/catalog/authorino-operator-catalog
-	cd $(PROJECT_DIR)/catalog && $(OPM) generate dockerfile authorino-operator-catalog
-catalog-dockerfile: $(CATALOG_DOCKERFILE) ## Generate catalog Dockerfile.
+	cd $(PROJECT_DIR)/catalog && $(OPM) generate dockerfile authorino-operator-catalog -i "quay.io/operator-framework/opm:v$(OPM_VERSION)-$(CATALOG_ARCH)"
+catalog-dockerfile: $(CATALOG_DOCKERFILE) ## Generate catalog dockerfile.
 
-# Generate the catalog file.
 $(CATALOG_FILE): $(OPM) $(YQ)
 	@echo "************************************************************"
-	@echo "Build authorino operator catalog"
+	@echo Build authorino operator catalog
 	@echo
-	@echo "BUNDLE_IMG = $(BUNDLE_IMG)"
-	@echo "CHANNELS   = $(CHANNELS)"
+	@echo BUNDLE_IMG					= $(BUNDLE_IMG)
+	@echo CHANNELS						= $(CHANNELS)
 	@echo "************************************************************"
 	@echo
-	@echo "Please check this matches your expectations and override variables if needed."
+	@echo Please check this matches your expectations and override variables if needed.
 	@echo
 	$(PROJECT_DIR)/utils/generate-catalog.sh $(OPM) $(YQ) $(BUNDLE_IMG) $@ $(CHANNELS)
 
@@ -30,47 +30,25 @@ catalog: $(OPM) ## Generate catalog content and validate.
 	# Initializing the Catalog
 	-rm -rf $(PROJECT_DIR)/catalog/authorino-operator-catalog
 	-rm -rf $(PROJECT_DIR)/catalog/authorino-operator-catalog.Dockerfile
-	$(MAKE) catalog-dockerfile
+	$(MAKE) $(CATALOG_DOCKERFILE)
 	$(MAKE) $(CATALOG_FILE) BUNDLE_IMG=$(BUNDLE_IMG)
 	cd $(PROJECT_DIR)/catalog && $(OPM) validate authorino-operator-catalog
 
-.PHONY: catalog-multiarch
-catalog-multiarch: $(OPM) ## Generate catalog content and validate for multiple architectures.
-	#Initializing the Catalog
-	@echo "Building catalog for architecture: $(arch)"
-	-rm -rf $(PROJECT_DIR)/catalog/authorino-operator-catalog
-	-rm -rf $(PROJECT_DIR)/catalog/authorino-operator-catalog.Dockerfile
-	-mkdir -p $(PROJECT_DIR)/catalog/authorino-operator-catalog
-	cd $(PROJECT_DIR)/catalog && $(OPM) generate dockerfile authorino-operator-catalog -i "quay.io/operator-framework/opm:v1.28.0-${arch}"
-	@echo "creating dir"
-	$(MAKE) $(CATALOG_FILE) BUNDLE_IMG=$(BUNDLE_IMG)
-	@echo "leaving dir"
-	cd $(PROJECT_DIR)/catalog && $(OPM) validate authorino-operator-catalog
-
-
-# Build a catalog image.
+# Build a catalog image by adding bundle images to an empty catalog using the operator package manager tool, 'opm'.
+# Ref https://olm.operatorframework.io/docs/tasks/creating-a-catalog/#catalog-creation-with-raw-file-based-catalogs
 .PHONY: catalog-build
 catalog-build: ## Build a catalog image.
 	# Build the Catalog
 	docker build $(PROJECT_DIR)/catalog -f $(PROJECT_DIR)/catalog/authorino-operator-catalog.Dockerfile -t $(CATALOG_IMG)
-
-.PHONY: catalog-build-multi
-catalog-build-multi: ## Build a multiarch catalog image.
-	# Build the Catalog
-	docker build $(PROJECT_DIR)/catalog -f $(PROJECT_DIR)/catalog/authorino-operator-catalog.Dockerfile -t $(IMG)
 
 # Push the catalog image.
 .PHONY: catalog-push
 catalog-push: ## Push a catalog image.
 	$(MAKE) docker-push IMG=$(CATALOG_IMG)
 
-# Deploy the catalog to a Kubernetes cluster.
-.PHONY: deploy-catalog
 deploy-catalog: $(KUSTOMIZE) $(YQ) ## Deploy operator to the K8s cluster specified in ~/.kube/config using OLM catalog image.
 	V="$(CATALOG_IMG)" $(YQ) eval '.spec.image = strenv(V)' -i config/deploy/olm/catalogsource.yaml
 	$(KUSTOMIZE) build config/deploy/olm | kubectl apply -f -
 
-# Undeploy the catalog from a Kubernetes cluster.
-.PHONY: undeploy-catalog
 undeploy-catalog: $(KUSTOMIZE) ## Undeploy controller from the K8s cluster specified in ~/.kube/config using OLM catalog image.
 	$(KUSTOMIZE) build config/deploy/olm | kubectl delete -f -
